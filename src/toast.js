32b88451d1eea8237b51967614596bb9d52693d8
DragoLeForgeron <galou2608@gmail.com>
2023-09-14 18:01:11 +0200

    Improve toast

--- src/toast.js
+++ src/toast.js
@@ -1,13 +1,13 @@
-function toastMe(element, message) {
+function toast(message) {
 
     const toast = document.createElement("div");
     toast.classList.add("toast");
     toast.innerText = message;
-    element.appendChild(toast);
+    document.body.appendChild(toast);
 
     toast.classList.add('toastLoad');
     setTimeout(() => {
         toast.remove();
-    }, 4000)
+    }, 4000);
 
 }
